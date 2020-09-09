94f961c8612eaebbe050b4b069e77cc1e2937b09
JennaTranter <jenna.tranter@gmail.com>
2020-09-09 13:20:01 -0700

    create basic to-do list functionality

--- app.js
+++ app.js
@@ -3,15 +3,39 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.get("/", function(req, res) {
 
   var today = new Date();
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res,send("Yay it's the weekend!");
-  } else {
-    res.send("Boo! I have to work!");
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 
